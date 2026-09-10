bc3c0f45376c3ddda1802790e0cf9756a6e7798a
sumit <skumit119644@gmail.com>
2026-09-10 18:32:15 +0530

    Add OpenAlex academic source provider

--- search_agent/__init__.py
+++ search_agent/__init__.py
@@ -5,6 +5,13 @@ from .source_selector import select_sources
 from .content_retriever import retrieve_selected_sources
 from .evidence_extractor import extract_research_evidence
 from .evidence_output import build_research_evidence_output, save_research_evidence
+from .source_providers import (
+    SourceProvider,
+    OpenAlexProvider,
+    register_provider,
+    get_provider,
+    search_academic,
+)
 
 __all__ = [
     "run_research",
@@ -12,5 +19,10 @@ __all__ = [
     "retrieve_selected_sources",
     "extract_research_evidence",
     "build_research_evidence_output",
-    "save_research_evidence"
+    "save_research_evidence",
+    "SourceProvider",
+    "OpenAlexProvider",
+    "register_provider",
+    "get_provider",
+    "search_academic",
 ]
